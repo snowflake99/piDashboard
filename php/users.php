f0004ada5aacfb12e61f41f8d448f2851cac0f3b
Sanjoy Saha <sanjoysaha85@gmail.com>
2017-06-11 13:07:48 -0400

    Adding new tiles for number of users

---
+++ php/users.php
@@ -0,0 +1,10 @@
+<?php
+    // Execute the command in shell
+    $shellOutput = shell_exec('uptime');
+  
+    // Get disk usage percentage
+    if (preg_match_all('/[0-9]*\susers/', $shellOutput, $matches))
+        echo explode(' ', $matches[0][0])[0];
+    else
+        echo "-";
+?>
